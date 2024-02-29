fb0a4df961a9ff22ec338bf0ace16b8b5c993f82
ajluvscats <aj_349@icloud.com>
2024-02-29 14:59:15 -0800

    iggikigig

---
+++ ScritpingProjectP7Gutierrez/Assets/Scripts/KeyInput.cs
@@ -0,0 +1,29 @@
+using Microsoft.Unity.VisualStudio.Editor;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyInput : MonoBehaviour
+{
+    public Image graphic;
+    public Sprite standard;
+    public Sprite downgfx;
+    public Sprite upgfx;
+    public Sprite heldgfx;
+    public Text boolDisplay1;
+    public Text boolDisplay2;
+    public Text boolDisplay3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        graphic.sprite = standard;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
