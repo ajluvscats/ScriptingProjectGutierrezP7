915937cab29610f6aca6a04534ee3adf0163b172
ajluvscats <aj_349@icloud.com>
2024-03-06 14:34:40 -0800

    scripting

--- ScritpingProjectP7Gutierrez/Assets/Scripts/AxisExample.cs
+++ ScritpingProjectP7Gutierrez/Assets/Scripts/AxisExample.cs
@@ -1,8 +1,5 @@
-using JetBrains.Annotations;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.AssetImporters;
 using UnityEngine;
+using System.Collections;
 using UnityEngine.UI;
 
 public class AxisExample : MonoBehaviour
@@ -10,13 +7,7 @@ public class AxisExample : MonoBehaviour
     public float range;
     public Text textOutput;
 
-    // Start is called before the first frame update
    
-
-
-
-
-    // Update is called once per frame
     void Update()
     {
 
@@ -24,7 +15,7 @@ public class AxisExample : MonoBehaviour
     float xPos = h * range;
 
     transform.position = new Vector3(xPos, 2f, 0);
-        Debug.Log("Value Returned: " + h.ToString("F2"));
+    Debug.Log("Value Returned: " + h.ToString("F2"));
 
     }
 }
